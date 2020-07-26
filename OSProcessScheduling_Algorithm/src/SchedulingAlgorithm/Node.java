cb2889711aced150531e26ff7e66d201ff004ae8
Administrator <nysyxxg@163.com>
2020-07-26 13:53:44 +0800

    os

--- OSProcessScheduling_Algorithm/src/SchedulingAlgorithm/Node.java
+++ OSProcessScheduling_Algorithm/src/SchedulingAlgorithm/Node.java
@@ -3,12 +3,13 @@ package SchedulingAlgorithm;
 public class Node {// 任务进程节点
     String name;  // 任务进程名称
     int priority; // 任务进程优先级
-    int runtime; // 任务进程运行时间
-    int arrivaltime; // 任务进程到达时间
-    int starttime; // 开始时间
-    int endtime;  //结束时间
-    int turntime;  //周转时间
-    double dturntime;  //带权周转时间
+    int runtime; // 任务进程运行时间--就是服务时间--serviceTime  例如 runtime = 100
+    int arrivaltime; // 任务进程到达时间 ，例如   arrivaltime = 3
+    int starttime; // 开始时间            例如   starttime = 102
+    int endtime;  //结束时间 = starttime + runtime = 102 + 100 = 202
+    int turntime;  //周转时间 = endtime - arrivaltime = 202 - 3 = 199
+    double dturntime;  //带权周转时间 = turntime / runtime = 199 / 100 = 1.99
+
     Node nextnode;// 下一个任务进程节点
     int statu;// 任务进程运行状态
     int newarrival;
