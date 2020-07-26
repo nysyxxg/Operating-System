391099ca52e1d93aee75939222db5d91796b4b7f
Administrator <nysyxxg@163.com>
2020-07-26 13:04:43 +0800

    os

--- OSProcessScheduling_Algorithm/src/SchedulingAlgorithm/Node.java
+++ OSProcessScheduling_Algorithm/src/SchedulingAlgorithm/Node.java
@@ -1,16 +1,16 @@
 package SchedulingAlgorithm;
 
-public class Node {// 任务节点
-    String name;  // 任务名称
-    int priority; // 任务优先级
-    int runtime; // 运行时间
-    int arrivaltime;
+public class Node {// 任务进程节点
+    String name;  // 任务进程名称
+    int priority; // 任务进程优先级
+    int runtime; // 任务进程运行时间
+    int arrivaltime; // 任务进程到达时间
     int starttime; // 开始时间
     int endtime;  //结束时间
     int turntime;  //周转时间
     double dturntime;  //带权周转时间
-    Node nextnode;
-    int statu;// 运行状态
+    Node nextnode;// 下一个任务进程节点
+    int statu;// 任务进程运行状态
     int newarrival;
     int newruntime;
 
